a4394686f74c21e65443e974b00aa1f5ed98f2c0
maurojcordoba <mauro.j.cordoba@gmail.com>
2021-06-24 13:10:39 -0300

    modificaciones varias

--- src/config.php
+++ src/config.php
@@ -1,12 +1,12 @@
 <?php 
     //API MovieDB
-    $apiKeyMovieDB    = '<api key>';
+    $apiKeyMovieDB    = 'ed6b300f1a94eb135e9323ea81178a45';
     $urlMovieDB       = 'https://api.themoviedb.org';
     $languageMovieDB  = 'es-ES';
 
 
     //API Telegram
-    $token = '<api key>';
+    $token = '1888619574:AAH_6DMxt5tFww4Y2vj_9fKbhYrjYcFJ4tA';
     $website = 'https://api.telegram.org/bot'.$token;
 
 
